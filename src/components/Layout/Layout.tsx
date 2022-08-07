7cce23a3ff2bff20024ec7deaa0a309fd6ab7b19
xiaoming <2604863701@qq.com>
2022-08-07 22:59:20 +0800

    Layout 组件 解决 重构去掉 let

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,28 +1,31 @@
 import React, {ReactElement} from 'react';
-import './layout.scss'
+import './layout.scss';
 import classes from '../../helper/classes';
 import Aside from './Aside';
 
 // 不知道 Props元素的类型，可以点击存在该类的进去查看
-interface Props extends React.HTMLAttributes<HTMLElement>{
-  children:ReactElement | Array<ReactElement>       // 限制使用者使用
+interface Props extends React.HTMLAttributes<HTMLElement> {
+  children: ReactElement | Array<ReactElement>       // 限制使用者使用
 }
 
 
-const Layout:React.FunctionComponent<Props> = (props) => {
-  const {className,children,...reset} = props
-  let hasAside = false
-  if((children as Array<ReactElement>).length){
-    (children as Array<ReactElement>).map(node=>{
-      if(node.type === Aside){
-        hasAside = true
-      }
-    })
-  }
+const Layout: React.FunctionComponent<Props> = (props) => {
+  const {className, children, ...reset} = props;
+  // let hasAside = false
+  // if((children as Array<ReactElement>).length){
+  // (children as Array<ReactElement>).map(node=>{
+  //   if(node.type === Aside){
+  //     hasAside = true
+  //   }
+  // }),
+  // }
 
-
+  const childrenAsArray = children as Array<ReactElement>;
+  const hasAside = childrenAsArray.length &&
+    childrenAsArray.reduce((result, node) =>
+    result || node.type === Aside, false);
   return (
-    <div className={classes("XM-Layout",hasAside ? 'hasAside' : '', className)} {...reset}>
+    <div className={classes('XM-Layout', hasAside ? 'hasAside' : '', className)} {...reset}>
       {props.children}
     </div>
   );
