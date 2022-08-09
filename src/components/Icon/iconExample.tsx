e55268d3ace3349ad62c73a05c39a19793f0c133
xiaoming <2604863701@qq.com>
2022-08-09 09:46:29 +0800

    icon 组件展示页面

--- src/components/Icon/iconExample.tsx
+++ src/components/Icon/iconExample.tsx
@@ -1,34 +1,21 @@
 import React from 'react';
-import styled from 'styled-components';
 import Icon from './Icon';
-const Wrapper = styled.div`
-     display: flex;
-     flex-direction: column;
-`;
-
 
 const IconExample = () => {
   return (
-    <Wrapper>
-      <header>
-        <div>
-          <h2>下面展示的是一个 icon 组件的例子</h2>
-        </div>
-      </header>
-      <main>
-        <div>
-          <span>这个组件有一个必须的参数【name】就是 svg 的名字.</span>
-          <span>也支持点击、鼠标、悬浮等各种事件，默认高宽 [1.4em]。</span>
-          <div className='ex-icon'>
-            <Icon name='settings' />
-            <Icon name='close' />
-            <Icon name='loading' />
-            <Icon name='thumbs-up' />
-          </div>
-        </div>
-      </main>
+    <div>
+      <div>
+        <span>这个组件有一个必须的参数【name】就是 svg 的名字.</span>
+        <span>也支持点击、鼠标、悬浮等各种事件，默认高宽 [1.4em]。</span>
+      </div>
+      <div className='ex-icon'>
+        <Icon name='settings' />
+        <Icon name='close' />
+        <Icon name='loading' />
+        <Icon name='thumbs-up' />
 
-    </Wrapper>
+      </div>
+    </div>
   );
 };
 
--- src/components/demo.tsx
+++ src/components/demo.tsx
@@ -1,8 +1,9 @@
 import React, {useState} from 'react';
 import Highlight,{defaultProps} from 'prism-react-renderer'
+import Icon from './Icon/Icon';
 
 interface Props {
-  code:string
+  code: string
 }
 
 const Demo:React.FunctionComponent<Props> =  (props)=>{
@@ -23,13 +24,26 @@ const Demo:React.FunctionComponent<Props> =  (props)=>{
     </Highlight>
   )
   return (
-    <div>
+    <div className='example'>
       <div>
-        {props.children}
+          <span>这个组件有一个必须的参数【name】就是 svg 的名字.</span>
+          <span>也支持点击、鼠标、悬浮等各种事件，默认高宽 [1.4em]。</span>
       </div>
 
-      <div>
-        <button onClick={()=>{setVisible(!visible)}}>展示代码</button>
+      <div className="iconCode">
+        <div className='ex-icon'>
+          <Icon name='settings' />
+          <Icon name='close' />
+          <Icon name='loading' />
+          <Icon name='thumbs-up' />
+          <Icon name='download' />
+        </div>
+
+        <div className="buttonCode" onClick={()=>{setVisible(!visible)}}>展示代码</div>
+
+
+      </div>
+      <div className="codeDiv">
         {visible && code}
       </div>
     </div>
